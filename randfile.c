879f34c1cfc05d47501262e49d84a7ed8df356fe
klau2000 <klau00@stuy.edu>
2019-11-09 01:02:56 -0500

    Initial function

--- randfile.c
+++ randfile.c
@@ -4,6 +4,30 @@
 #include <fcntl.h>
 #include <errno.h>
 
-int main() {
+int randnum(){
+  int fd = open("/dev/random", 0_RDONLY);
+  int buff;
+  read(fd, &buff, sizeof(int));
 
+  if(errno){ //see if any error
+    printf("Error number: %d\n", errno);
+  }
+  return buff;
+}
+int main() {
+  int fd = open("randnum.c", 0_RDWR | 0_CREAT, 644);
+  int buff[11];
+  printf("Generating random numbers:\n");
+  for(int i = 0; i < 10; i++){
+    buff[i] = randnum();
+    printf("random %d: %d\n", i, buff[i]);
+  }
+  if(errno){ //see if any error
+    printf("Error number: %d\n", errno);
+  }
+  printf("Writing numbers to file...\n\n");
+  write(fd,buff, sizeof(buff));
+  printf("Reading numbers from file...\n\n");
+  printf("Verification that written values were the same:\n");
+  
 }

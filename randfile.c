8dcae640f98417f1543dd044611a5f9a0512763b
klau2000 <klau00@stuy.edu>
2019-11-09 21:27:15 -0500

    edits

--- randfile.c
+++ randfile.c
@@ -16,7 +16,7 @@ int randnum(){
   return buff;
 }
 int main() {
-  int file = open("randnum.txt", O_RDWR | O_CREAT, 666);
+  int fd = open("randnum.txt", O_RDWR | O_CREAT, 666);
   int buff[11];
   printf("Generating random numbers:\n");
   for(int i = 0; i < 10; i++){
@@ -27,18 +27,17 @@ int main() {
     printf("Error number: %d\n", errno);
   }
   printf("Writing numbers to file...\n\n");
-  write(file,buff, sizeof(buff));
-
+  write(fd,buff, sizeof(buff));
   printf("Reading numbers from file...\n\n");
 
   printf("Verification that written values were the same:\n");
   int buff2[11];
-  read(file, &buff2, sizeof(buff2));
+  read(fd, &buff2, sizeof(buff2));
   for(int j = 0; j < 10; j++){
     printf("random %d: %d\n", j, buff2[j]);
   }
   if(errno){ //see if any error
     printf("Error number: %d\n", errno);
   }
-  close(file);
+  close(fd);
 }
